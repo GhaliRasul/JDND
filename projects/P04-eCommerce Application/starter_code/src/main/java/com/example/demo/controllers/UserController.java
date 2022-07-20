258689b6d3471254a63e22834d0e64394ad5b40e
GhaliRassul <rasulghali@gmail.com>
2022-07-20 14:06:26 +0200

    after JWT

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,11 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,13 +19,16 @@ import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
 
+import javax.sound.sampled.Line;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+	private static final Logger log= LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserRepository userRepository;
-	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	@Autowired
 	private CartRepository cartRepository;
 
@@ -46,6 +50,13 @@ public class UserController {
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
+
+		if(createUserRequest.getPassword().length()<8 ||
+				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
+			return ResponseEntity.badRequest().build();
+		}
+		log.info(createUserRequest.getUsername()+" is username");
+		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 		userRepository.save(user);
 		return ResponseEntity.ok(user);
 	}
